aeede5cd49635632b8a0c0872d4b8e4d5909c417
Jovid Nurov <jovid.9@gmail.com>
2020-10-05 17:14:11 +0500

    adding second function for rejecting a payment

--- cmd/main.go
+++ cmd/main.go
@@ -9,10 +9,12 @@ import (
 func main() {
 
 	svc := &wallet.Service{}
+
 	//wallet.RegisterAccount(svc, "+992907013487")
 	//svc.RegisterAccount("+992907013487")
 	//svc.Deposit(1, 10)
 	account, err := svc.RegisterAccount("+992907013487")
+
 	if err != nil {
 		fmt.Println(*svc)
 		return
@@ -26,10 +28,10 @@ func main() {
 
 	payment, err := svc.Pay(account.ID, 88, "some_category")
 	findAccount, err := svc.FindAccountByID(2)
-	findPayment, err := svc.FindPaymentByID("1")
+	findPayment, err := svc.FindPaymentByID("88")
 	fmt.Println(account)
 	fmt.Println(account.Balance, account.ID)
-	fmt.Println(payment)
+	fmt.Println(payment.Amount)
 	fmt.Println(findAccount)
 	fmt.Println(findPayment)
 
